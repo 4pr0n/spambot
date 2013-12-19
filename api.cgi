ebe068539fa6730a756a156aaac2a757e448f868
4pr0n <1fakeyfake@gmail.com>
2013-12-19 00:54:25 -0800

    initial commit for UI/web stuff. Sort of works, kind of.

---
+++ api.cgi
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+
+from json import dumps
+from traceback import format_exc # Stack traces
+from cgi   import FieldStorage # Query keys
+from cgitb import enable as cgi_enable; cgi_enable() # for debugging
+
+def main():
+	'''
+		Returns JSON-formatted data.
+	'''
+	keys = get_keys()
+	if not 'method' in keys:
+		return {'error':'unspecified method'}
+	method = keys['method']
+	if   method == 'get_scoreboard': return get_scoreboard()
+	elif method == 'get_removed':    return get_removed(keys)
+	elif method == 'get_filter':     return get_filter(keys)
+
+def get_scoreboard():
+	from py.DB import DB
+	db = DB()
+	result = []
+	for (username, score) in db.select('username, score', 'admins'):
+		if score == 0: continue
+		result.append({
+			'user' : username,
+			'score' : score
+		})
+	result = sorted(result, key=lambda x: x['score'], reverse=True)
+	return {
+		'scoreboard': result
+	}
+
+def get_removed(keys):
+	start = int(keys.get('start',  0))
+	count = int(keys.get('count', 10))
+	from py.DB import DB
+	db = DB()
+	cursor = db.conn.cursor()
+	q = '''
+			select
+				type, text, author, isspam, posttype, permalink, date
+				from 
+					filters
+				inner join 
+					log_removed
+				on
+					log_removed.filterid = filters.id
+				order by date desc
+				limit %d
+				offset %d
+		''' % (count, start)
+	result = []
+	for (spamtype, spamtext, author, isspam, posttype, permalink, date) in \
+			cursor.execute(q):
+		if author == '': author = 'internal'
+		result.append({
+			'spamtype'  : spamtype,
+			'spamtext'  : spamtext,
+			'author'    : author,
+			'is_spam'   : (isspam == 1),
+			'posttype'  : posttype,
+			'permalink' : permalink,
+			'date'      : date
+		})
+	cursor.close()
+	return {
+			'removed' : result,
+			'start' : start + count,
+			'count' : count
+		}
+
+def get_filter(keys):
+	start = int(keys.get('start',  0))
+	count = int(keys.get('count', 10))
+	from py.DB import DB
+	db = DB()
+	cursor = db.conn.cursor()
+	q = '''
+			select
+				type, text, user, action, date
+				from 
+					filters
+				inner join 
+					log_filters
+				on
+					log_filters.filterid = filters.id
+				order by date desc
+				limit %d
+				offset %d
+		''' % (count, start)
+	result = []
+	for (spamtype, spamtext, user, action, date) in \
+			cursor.execute(q):
+		if user == '': user = 'internal'
+		result.append({
+			'spamtype'  : spamtype,
+			'spamtext'  : spamtext,
+			'user'      : user,
+			'action'    : action,
+			'date'      : date
+		})
+	cursor.close()
+	return {
+			'filter_changes' : result,
+			'start' : start + len(result),
+			'count' : count
+		}
+
+def get_keys(): # Get query keys
+	form = FieldStorage()
+	keys = {}
+	for key in form.keys():
+		keys[key] = form[key].value
+	return keys
+
+########################
+# ENTRY POINT
+if __name__ == '__main__':
+	print "Content-Type: application/json"
+	print ""
+	try:
+		print dumps(main(), indent=2)
+	except Exception, e:
+		# Return stacktrace
+		print dumps({'error': str(format_exc())})
+	print "\n\n"
---
+++ ui/js/main.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+	// Enable side-bar toggling
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+	getScoreboard();
+	getRemovedSpam();
+	getFilterChanges();
+});
+
+function getScoreboard() {
+	$.getJSON('api.cgi?method=get_scoreboard')
+		.fail(function() {
+			// TODO handle failure
+		})
+		.done(function(json) {
+			if (json.error !== undefined) {
+				// TODO Error handler
+				return;
+			}
+			$('<tr/>')
+				.appendTo( $('#scoreboard') )
+				.append( $('<th>#</th>') )
+				.append( $('<th>admin</th>') )
+				.append( $('<th>score</th>') );
+			// Build scoreboard
+			$.each(json.scoreboard, function(index, item) {
+				$('<tr/>')
+					.click(function() {
+						// TODO Direct to user page
+					})
+					.appendTo( $('#scoreboard') )
+					.append( $('<td/>').html(index + 1 ) )
+					.append( $('<td/>').html(item.user ) )
+					.append( $('<td/>').html(item.score) );
+			});
+		});
+}
+
+function getRemovedSpam(start, count) {
+	if (start === undefined) start =  0;
+	if (count === undefined) count = 10;
+	$.getJSON('api.cgi?method=get_removed&start=' + start + '&count=' + count)
+		.fail(function() {
+			// TODO handle failure
+		})
+		.done(function(json) {
+			if (json.error !== undefined) {
+				// TODO Error handler
+				return;
+			}
+			$('#removed-spam').empty();
+			$('<tr/>')
+				.appendTo( $('#removed-spam') )
+				.append( $('<th>date</th>') )
+				.append( $('<th>reddit</th>') )
+				.append( $('<th>type</th>') )
+				.append( $('<th>filter text</th>') )
+				.append( $('<th>spam?</th>') )
+				.append( $('<th>credit</th>') );
+			// Build scoreboard
+			$.each(json.removed, function(index, item) {
+				$('<tr/>')
+					.click(function() {
+						// TODO Error handling
+					})
+					.appendTo( $('#removed-spam') )
+					.append( $('<td/>').html(new Date(item.date * 1000).toLocaleString() ) )
+					.append( $('<td/>').html( $('<a/>').attr('href', item.permalink).html(item.posttype) ) )
+					.append( $('<td/>').html(item.spamtype) )
+					.append( $('<td/>').html(item.spamtext) )
+					.append( $('<td/>').html(item.is_spam ) )
+					.append( $('<td/>').html(item.author  ) );
+			});
+
+			// Back/next buttons
+			if (start >= 10) {
+				$('#removed-spam-back')
+					.removeAttr('disabled')
+					.unbind('click')
+					.click(function() {
+						getRemovedSpam(json.start - 20, json.count);
+					});
+			} else {
+				$('#removed-spam-back')
+					.attr('disabled', 'disabled');
+			}
+			$('#removed-spam-next')
+				.unbind('click')
+				.click(function() {
+					getRemovedSpam(json.start, json.count);
+				});
+		});
+}
+
+function getFilterChanges(start, count) {
+	if (start === undefined) start =  0;
+	if (count === undefined) count = 10;
+	$.getJSON('api.cgi?method=get_filter&start=' + start + '&count=' + count)
+		.fail(function() {
+			// TODO handle failure
+		})
+		.done(function(json) {
+			if (json.error !== undefined) {
+				// TODO Error handler
+				return;
+			}
+			$('#filter-changes').empty();
+			$('<tr/>')
+				.appendTo( $('#filter-changes') )
+				.append( $('<th>date</th>') )
+				.append( $('<th>user</th>') )
+				.append( $('<th>action</th>') )
+				.append( $('<th>type</th>') )
+				.append( $('<th>filter text</th>') );
+			$.each(json.filter_changes, function(index, item) {
+				$('<tr/>')
+					.click(function() {
+						// TODO Error handling
+					})
+					.appendTo( $('#filter-changes') )
+					.append( $('<td/>').html(new Date(item.date * 1000).toLocaleString() ) )
+					.append( $('<td/>').html(item.user    ) )
+					.append( $('<td/>').html(item.action  ) )
+					.append( $('<td/>').html(item.spamtype) )
+					.append( $('<td/>').html(item.spamtext) );
+			});
+
+			// Back/next buttons
+			if (start >= 10) {
+				$('#filter-changes-back')
+					.removeAttr('disabled')
+					.unbind('click')
+					.click(function() {
+						getFilterChanges(json.start - 20, json.count);
+					});
+			} else {
+				$('#filter-changes-back')
+					.attr('disabled', 'disabled');
+			}
+			$('#filter-changes-next')
+				.unbind('click')
+				.click(function() {
+					getFilterChanges(json.start, json.count);
+				});
+		});
+}
